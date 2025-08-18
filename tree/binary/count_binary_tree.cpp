7aa3e829da9e9f91c412fa6fb59a1131d1ebc664
siffahim <siffahim25gmail.com>
2025-08-18 16:05:40 +0600

    binary

--- tree/binary/count_binary_tree.cpp
+++ tree/binary/count_binary_tree.cpp
@@ -4,20 +4,42 @@ using namespace std;
 class Node{
     public:
         int val;
-        Node *next;
-        Node *prev;
+        Node *left;
+        Node *right;
     Node(int val){
         this->val = val;
-        this->next = NULL;
-        this->prev = NULL;
+        this->left = NULL;
+        this->right = NULL;
     }
 };
 
 int input_tree(){
     int val;
     cin >> val;
-    
+    Node *root;
+    if(val == -1) 
+        root = NULL;
+    else
+        root = new Node(val);
+
+    queue<Node *> q;
+    q.push(root);
+
+    while(!q.empty()){
+        Node *p = q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+
+        if(p->left)
+            q.push(p->left);
+        if(p->right)
+            q.push(p->right);
+    }
 }
+
+
 
 
 
